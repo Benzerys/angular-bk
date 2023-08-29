c0baa5f95cc509652cd01c6d6d65aa8cfa6a0871
Benz <Benz>
2023-08-29 15:37:17 +0700

    bk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'navbar',
+    component: NavbarComponent,
+  },
+  {
+    path: 'post',
+    component: PostComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { PostComponent } from './post/post.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-learn-yt8h';
+  parentMessage: string = 'Message Changed';
+  message!: string;
+  fromChildOutput!: string;
+  animals = [1, 2, 3, 3, 4, 5];
+
+  @ViewChild(PostComponent) childComp: any;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.childComp);
+    this.message = this.childComp.childMessage;
+    let number = this.animals.filter((value) => value === 3);
+    // number = [...new Set(this.animals)];
+    console.log(number);
+
+    console.log(this.animals);
+  }
+
+  reciveMessage($event: any) {
+    this.fromChildOutput = $event;
+  }
 }
